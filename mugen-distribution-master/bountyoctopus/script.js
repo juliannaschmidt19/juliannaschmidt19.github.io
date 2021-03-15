8bd1f1f2d0362cd6b33194fa22dfd74f619742d3
juliannaschmidt19 <70173877+juliannaschmidt19@users.noreply.github.com>
2021-03-15 02:24:55 -0400

    Delete mugen-distribution-master/bountyoctopus directory

--- mugen-distribution-master/bountyoctopus/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 500, 
-  	timeBetweenNPCs: 3200, npcCollisionRadius: 300, actionDuration: 600, jumpHeight: 350});
-});

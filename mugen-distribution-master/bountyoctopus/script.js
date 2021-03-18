750aa72e76b5a517164d1ff1572b6af72dbc0a61
juliannaschmidt19 <70173877+juliannaschmidt19@users.noreply.github.com>
2021-03-18 00:00:55 -0400

    Add files via upload

--- mugen-distribution-master/bountyoctopus/script.js
+++ mugen-distribution-master/bountyoctopus/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 500, 
-  	timeBetweenNPCs: 3200, npcCollisionRadius: 300, actionDuration: 600, jumpHeight: 350});
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, 
+  	timeBetweenNPCs: 3200, npcCollisionRadius: 100, actionDuration: 600, jumpHeight: 350, });
 });
